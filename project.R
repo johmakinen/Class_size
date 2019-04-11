a4b055dca303372c8f17582b416febfd195eafc4
johmakinen <joh.makinen@gmail.com>
2019-04-11 17:38:22 +0300

    syntax

--- project.R
+++ project.R
@@ -1,12 +1,12 @@
-setwd("C:/Users/johma/Desktop/kouluhommii/Statistical Inference/Projekti")
-
 
 library(Ecdat)
 library(dplyr)
 library(ggplot2)
 data(Star)
+
 # Get Mathematics Scores and the classroom size from the whole data set.
 df = Star[,c(1,3)]
+
 #Split data into small and regular sized classrooms and get he math scores for both of them.
 small = filter(df,classk == "small.class")$tmathssk
 regular = filter(df, classk== "regular")$tmathssk
